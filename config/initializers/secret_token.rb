2e2e31d12029fadbcc5ecbc725daafdc0fd90aa0
@xpater <tomarobaski@gmail.com>
2016-06-27 08:30:22 +0000

    innitital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grayscale::Application.config.secret_key_base = '5f75e2203cf02f7a1694e6bf8ac9c0409c01d4747ba553fd92cc97d6f0b43651f0b12b764aed853cc70def7a5e46f910fc14677f7324fb22e8adbee67fb84ed2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grayscale::Application.config.session_store :cookie_store, key: '_grayscale_session'
